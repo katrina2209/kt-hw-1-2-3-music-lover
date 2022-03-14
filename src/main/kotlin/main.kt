965f4bbc1435bbea492371a0313d51991bcdef09
katrina2209 <katrina22@mail.ru>
2022-03-14 21:36:34 +0300

    Output format changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
 fun main() {
     val previousSum = 10_000
-    val buyingSum = 300
+    val buyingSum = 332
     val isMeloman = true
     val sumWithDiscount = if (previousSum <= 1_000) buyingSum else if (previousSum >= 10_001) buyingSum * 95 / 100 else if (buyingSum - 100 > 0) buyingSum - 100 else 0
     val result = if (isMeloman) sumWithDiscount * 99 / 100 else sumWithDiscount
 
-    println("Сумма покупки с учетом всех скидок: $result")
+    println("Сумма покупки с учетом всех скидок: $result рублей ${result % 100} коп.")
 }
