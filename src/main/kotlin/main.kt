1bf6fb8f1707c55fcdbe0425eab9fe265b6002e2
katrina2209 <katrina22@mail.ru>
2022-03-14 22:39:30 +0300

    Discount calculation changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,8 @@ fun main() {
     val previousSum = 10_002
     val buyingSum = 399.00
     val isMeloman = true
-    val discount = if (previousSum >= 10_001) buyingSum * 0.05 else if (buyingSum - 100 > 0) 100.00 else 0.00
-    val sumWithDiscount = buyingSum - discount
+    val discount = if (previousSum >= 10_001) buyingSum * 0.05 else if (previousSum in 1001..10000) 100.00 else 0.00
+    val sumWithDiscount = if (buyingSum - discount > 0) buyingSum - discount else 0.00
     val result = (if (isMeloman) sumWithDiscount * 99 else sumWithDiscount * 100)
 
     println("Сумма покупки с учетом всех скидок: ${(result / 100).toInt()} руб. ${(result % 100).roundToInt().toInt()} коп.")
